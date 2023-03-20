818d49e35d82d1d732b208cc9d86818f32bbcd86
david-aryasetia <david.aryasetia@gmail.com>
2023-03-20 15:20:00 +0700

    setup tailwind cdn

--- index.php
+++ index.php
@@ -4,14 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>SIAPT</title>
+  
+
 </head>
 <body>
-    <p class="text-3xl font-bold underline">Heldddsdfsdfsdflo</p>
+  
 
-    <?php
+    <p class="text-3xl font-bold text-red-500">
+        <?php
     echo "Test"
     ?>
+    </p>
+    
     
 </body>
 </html>
