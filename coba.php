2d6788a8636e81710fe3806fd1143d3647b5dbc9
david-aryasetia <david.aryasetia@gmail.com>
2023-03-28 13:47:05 +0700

    Setup Project

---
+++ coba.php
@@ -0,0 +1,5 @@
+<?php
+$kategori = "heloo world";
+
+echo $kategori;
+?>
--- index.php
+++ index.php
@@ -1,24 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title>SIAPT</title>
-  
-
-</head>
-<body>
-  
-
-    <p class="text-3xl font-bold text-red-500">
-        <?php
-    echo "Test";
-    phpinfo();
-    ?>
-    </p>
-    
-    
-</body>
-</html>
+<?php
+	require "includes/config.inc.php";
+	require "includes/func.inc.php";
+	echo "Hello SIAPT Hello";
+?>
