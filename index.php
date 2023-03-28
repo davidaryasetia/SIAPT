b214955c53dc5d4192bfedccf8fe9585b9f3cfc0
david-aryasetia <david.aryasetia@gmail.com>
2023-03-28 15:42:42 +0700

    update package

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
-<?php
-	require "includes/config.inc.php";
-	require "includes/func.inc.php";
-	echo "Hello SIAPT Hello";
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/src/input.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $testing = "Testing Tailwind";
+
+   echo $testing
+    ?>
+    <p class="text-red-600">Red Test Tailwind & Auto Synchcronize</p>
+
+</body>
+
+</html>
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,php,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
