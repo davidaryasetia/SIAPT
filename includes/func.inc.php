c4bb49f78e95c52d5702e57dff848b41be6024cc
David Aryasetia <david.aryasetia@gmail.com>
2024-02-23 13:06:50 +0700

    update

--- includes/func.inc.php
+++ includes/func.inc.php
@@ -5,15 +5,20 @@
  * @param  string $db_pass password untuk login db
  * @return oci_resource          objek koneksi db
  */
-$db_user="PA0001";
-$db_pass="726987";
+$db_user = "PA0001";
+$db_pass = "726987";
 
 function konekDb($db_user, $db_pass) {
-    $con=oci_connect($db_user,$db_pass,"10.252.209.213/orcl.mis.pens.ac.id");
-    if(!$con) {
+    $con = mysqli_connect("localhost", $db_user, $db_pass, "nama_database");
+    // Ganti "localhost" dengan host MySQL Anda dan "nama_database" dengan nama database yang digunakan.
+
+    if (!$con) {
         responseError('ERR-DB');
+    } else{
+        echo "nromal";
     }
-	return $con;
-	oci_close($con);
+    return $con;
+    // Jangan letakkan oci_close($con); di sini, karena kode di bawah return tidak akan dieksekusi.
 }
+
 ?>
